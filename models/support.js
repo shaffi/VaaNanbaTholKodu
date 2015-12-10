c4b1727bc7a2b74052152a6761488e4987f871c7
PrasanthJayaraman <prasanthj@outlook.com>
2015-12-10 10:48:17 +0530

    Added support schema

---
+++ models/support.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var supportSchema = new mongoose.Schema({
+  name: String,
+  mobile: Number,
+  altnum: Number,
+  blood: String,
+  time_from: String,
+  time_to: String,
+  address: String,
+  type: String
+}, { collection: 'support'});
+
+var Support = mongoose.model('support', supportSchema);
+
+module.exports = Support;
